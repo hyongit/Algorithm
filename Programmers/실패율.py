8af9aa18f1bd20c9d8b1553b752c9902ea85fcb6
Hyoeun Park <phyoeun57@gmail.com>
2022-02-03 01:36:32 +0900

    STUDY HARD

---
+++ Programmers/실패율.py
@@ -0,0 +1,26 @@
+stages = [2, 1, 2, 6, 2, 4, 3, 3]
+N = 5
+
+stages.sort()
+tmp = []
+new_tmp = []
+answer = []
+
+for i in range(1, N + 1):
+    n = 0
+    for j in stages:
+        if j <= i:
+            n += 1
+    if n == 0:
+        tmp.append(0)
+    else:
+        tmp.append(n / len(stages))
+    stages = stages[n:]
+
+new_tmp = sorted(tmp)[::-1]
+
+for j in range(len(new_tmp)):
+    answer.append(tmp.index(new_tmp[j]) + 1)
+    tmp[tmp.index(new_tmp[j])] = 2 # 원소를 2로 바꿔줌으로써 중복 방지 !
+
+print(answer)
