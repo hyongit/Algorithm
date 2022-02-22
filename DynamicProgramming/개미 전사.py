1fa082c75a724db9d58dbfc98ad79f67246ef8d8
Hyoeun Park <phyoeun57@gmail.com>
2022-02-22 21:22:49 +0900

    STUDY HARD

--- DynamicProgramming/개미 전사.py
+++ DynamicProgramming/개미 전사.py
@@ -1,14 +1,13 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-# DP테이블 초기화
-d = [0] * 100
-
-#다이나믹 프로그래밍 진행 (보텀업)
+d = [0] * n
 d[0] = arr[0]
-d[1] = max(arr[0], arr[1])
+d[1] = arr[1]
 
 for i in range(2, n):
-    d[i] = max(d[i-1], d[i-2] + arr[i])
+    tmp = 0
+    d[i] = max(d[i-1], d[i-2]+arr[i])
 
-print(d[n-1])
+print(d) # 1 3 3 8
+print(max(d)) # 8
