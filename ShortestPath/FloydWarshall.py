7729b0e3323c189a5909e260a15ed1ef233a9c12
Hyoeun Park <phyoeun57@gmail.com>
2022-03-19 01:13:27 +0900

    STUDY HARD

---
+++ ShortestPath/FloydWarshall.py
@@ -0,0 +1,30 @@
+#플로이드 워셜
+#'바로 이동하는 거리'가 '특정한 노드를 거쳐서 이동하는 거리'보다 크면 이를 더 작은 값으로 갱신
+import sys
+
+input = sys.stdin.readline
+INF = int(1e9)
+
+n = int(input())
+m = int(input())
+
+graph = [[INF] * (n+1) for _ in range(n+1)]
+
+#자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if a == b:
+            graph[a][b] = 0
+
+#각 간선에 대한 정보를 입력받아, 그 값으로 초기화
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+
+#점화식에 따라 플로이드 워셜 알고리즘 수행
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+print(graph)
