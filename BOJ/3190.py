9ff469e9585afc8cca8e8659fc20126301f3a5cc
Hyoeun Park <phyoeun57@gmail.com>
2022-05-19 15:59:32 +0900

    STUDY HARD

--- BOJ/3190.py
+++ BOJ/3190.py
@@ -1,56 +1,90 @@
 # 백준 [뱀]
 # 시뮬레이션
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n = int(input())
+k = int(input())
+
+# 맵 정보
+maps = [[0]*n for _ in range(n)]
+
+for _ in range(k):
+    a, b = map(int, input().split())
+    maps[a-1][b-1] = 1
+
+# 방향 전환 정보
+l = int(input())
+times = []
+for _ in range(l):
+    x, c = input().split()
+    times.append([int(x), c])
+
+# 처음에 동쪽을 보고 있으므로 (동 0, 남 1, 서 2, 북 3)
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
 
 def turn(d, c):
     # 동, 남, 서, 북 (0, 1, 2, 3)
     # 왼쪽으로 90도 회전 0 -> 3 -> 2 -> 1
-    
+    if c == 'L':
+        d = (d - 1) % 4
     # 오른쪽으로 90도 회전 0 -> 1 -> 2 -> 3
+    else:
+        d = (d + 1) % 4
     
+    return d
 
 def simulate():
-    # 뱀의 머리 위치
-    
+    # 초기 뱀의 위치
+    x, y = 0, 0
     # 뱀이 존재하는 위치는 2로 표시
-   
+    maps[x][y] = 2
     # 처음에는 direction 동쪽(0)
-    
+    direction = 0
     # 시작한 뒤로부터 흐른 시간
-    
-    # 다음에 회전할 정보
-    
+    time = 0
+    idx = 0
     # 뱀 (큐로 표현)
+    q = deque([(x, y)])
 
-    
+    while True:
         # 이동한 위치 계산
-        
+        nx = x + dx[direction]
+        ny = y + dy[direction]
         # 이동한 위치가 맵 범위 안에 있고, 뱀의 몸통이 없는 위치일 때
-        
+        if 0 <= nx < n and 0 <= ny < n and maps[nx][ny] != 2:
             # 사과가 없다면 이동 후 꼬리 제거
-            
+            if maps[nx][ny] != 1:
                 # 뱀 머리 이동
-                
+                maps[nx][ny] = 2
+                q.append([nx, ny])
                 #꼬리 제거
-                
+                temp_x, temp_y = q.popleft()
+                maps[temp_x][temp_y] = 0
+
             #사과가 있다면 이동 후 꼬리 그대로
-            
+            if maps[nx][ny] == 1 :
+                maps[nx][ny] = 2
+                q.append([nx, ny])
+
         #벽이나 몸통에 부딪혔다면
-        
+        else :
+            time += 1
+            break
 
         # 실행할 때마다 time 및 뱀 머리 이동
-        
+        x, y = nx, ny
+        #print(x, y)
+        time += 1
 
         #회전할 시간의 경우
-        
+        if idx < l and  time == times[idx][0]:
+            direction = turn(direction, times[idx][1])
+            idx += 1
+    
+    return time
 
-
-# 맵 정보
-
-# 방향 전환 정보
-
-# 처음에 동쪽을 보고 있으므로 (동 0, 남 1, 서 2, 북 3)
-
-
-# 사과가 있는 곳은 1로
-
-
+print(simulate())
--- BOJ/9663.py
+++ BOJ/9663.py
@@ -1,38 +1,43 @@
 # 백준 [N-Queen]
 # 백트래킹 : break나 continue해서 불필요한 재귀 호출을 줄임
 
-n = int(input())
+# n = int(input())
+n = 8
+answer = 0
+
 row = [0] * n
-visited = [0] * n
+visit = [0] * n
 
-result = 0
+def recur(num):
+    global answer
 
-def dfs(depth):
-    global result
-
-    if depth == n: # 종료 조건, depth가 n까지 갔을 때
-        result += 1
+    if num == n:
+        answer += 1
         return
-
+    
     for i in range(n):
-        # 자리에 Queen이 존재하는지 확인
-        if visited[i] == 0 :    
-            row[depth] = i
+        # 방문 안한 것 확인
+        if visit[i] == 0:
+            # Queen 놓기
+            row[num] = i
 
             pos = True
-            # depth만큼 for문 돌리면서 대각선에 Queen이 존재 검사 --> 존재 시 break, 존재하지 않으면 depth 늘려 탐색
-            for j in range(depth):
-                # 대각선 존재 여부 : 행 차이 == 열 차이
-                if abs(depth - j) == abs(row[depth] - row[j]):
+
+            # 깊이 만큼 for문 돌리면서 대각선에 Queen 존재 여부 검사 
+            # --> 존재 시 break, 존재하지 않으면 깊이 늘려서 탐색
+            for j in range(num):
+                # 대각선 존재 여부 절댓값 행 차이 == 절댓값 열 차이 
+                if abs(num - j) == abs(row[num] - row[j]) :
                     pos = False
                     break
+            
+            # 대각선 중복 없을 시 
+            if pos:
+                #방문 처리
+                visit[i] = 1
+                recur(num+1)
+                visit[i] = 0
 
-            # 중복 없을 시
-            if pos :
-                visited[i] = True
-                dfs(depth + 1)
-                visited[i] = 0
 
-
-dfs(0)
-print(result)
+recur(0)
+print(answer)
