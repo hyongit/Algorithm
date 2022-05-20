fe66368d224bf3fa92853c635d78fcee59b3fd61
Hyoeun Park <phyoeun57@gmail.com>
2022-05-20 19:32:03 +0900

    STUDY HARD

--- BOJ/1697.py
+++ BOJ/1697.py
@@ -1,1 +1,26 @@
-# 백준 [숨바꼭질]
+# 백준 [연결 요소의 개수]
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+visit = [0]*(n+1)
+answer = 0
+
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def dfs(num):
+
+    visit[num] = 1
+    
+    for i in graph[num]:
+        if visit[i] == 0:
+            dfs(i)
+
+for i in range(1, n+1):
+    if visit[i] == 0:
+        dfs(i)
+        answer += 1
+
+print(answer)
