f905314e1eb9b38ee112518b094dc265a1cb1e76
Hyoeun Park <phyoeun57@gmail.com>
2022-07-18 00:45:36 +0900

    no message

--- BOJ/9095.py
+++ BOJ/9095.py
@@ -1,26 +1,21 @@
 # [백준] 1,2,3 더하기
 # DP
 
-n = int(input())
-nums = []
+t = int(input())
 
-for _ in range(n):
-    num = int(input())
-    nums.append(num)
-
-def dynamic(x):
-
-    if x == 1:
+def dynamic(num):
+    if num == 1:
         return 1
-    
-    elif x == 2:
+
+    elif num == 2:
         return 2
-    
-    elif x == 3:
+
+    elif num == 3:
         return 4
     
     else:
-        return dynamic(x-1) + dynamic(x-2) + dynamic(x-3)
+        return dynamic(num-1) + dynamic(num-2) + dynamic(num-3)
 
-for i in nums:
-    print(dynamic(i))    
+for _ in range(t):
+    n = int(input())
+    print(dynamic(n))
