1bd52a691a7e392e67a19ef419a40ad4e5adb18a
Hyoeun Park <phyoeun57@gmail.com>
2022-08-03 00:01:28 +0900

    STUDY HARD

--- Programmers/문자열 압축.py
+++ Programmers/문자열 압축.py
@@ -1,30 +1,26 @@
-s = 'abcabcdede'
-
-for i in range(1, len(s)+1): # i자씩 끊기!
+def solution(s):
+    answer = []
+    
+    for i in range(1, len(s)+1):
         b = ''
-        cnt = 1 
-        tmp=s[:i] 
-        # [a]
-        # [ab]
-        # [abc]
-        # ...
-
-        for j in range(i, len(s)+i, i): # i부터 끝까지 i단위로 나누어 탐색
-            print(j, s[j:i+j])
-            if tmp==s[j:i+j]:# i 단위로 자른 것이 앞과 같으면
-
-                # i = 1, [b, c, a, b, c, ...]
-                # i = 2, [ca, bc, de, de]
-                # i = 3, [abc, ded, e]
-
-                cnt+=1 # 카운트
-            else: # 아니면
-                if cnt!=1: # 앞에서 중복이 있었다!
-                    b = b + str(cnt)+tmp # 글자 만들기
-                    #print(j, b)
-                else:
-                    b = b + tmp # 아니면 그냥 문자 붙임
+        cnt = 1
+        tmp = s[:i]
         
-                tmp=s[j:j+i]
+        for j in range(i, len(s)+i, i):
+            if tmp == s[j:j+i]:
+                cnt += 1
+                
+            else:
+                if cnt > 1:
+                    b = b + str(cnt) + tmp
+                    
+                else:
+                    b += tmp
+                    
+                tmp = s[j:j+i]
                 cnt = 1
-            #print(b)
+    
+        answer.append(len(b))
+    return min(answer)
+
+print(solution(input()))
